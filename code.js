155d1e7030a92a7cf393d28c360c2eb9849dec97
gbrown20 <gbrown20@DESKTOP-2J0M7L5.localdomain>
2023-11-07 18:37:19 -0700

    Working on runtime

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    const visited = new Set();
+    const path = [start];
+    let current = start;
+
+    while (current !== end) {
+        visited.add(current);
+        let foundNext = false;
+
+        for (const neighbor in graph[current]) {
+            if (!visited.has(neighbor) && graph[current][neighbor] > 0) {
+                path.push(neighbor);
+                current = neighbor;
+                foundNext = true;
+                break;
+            }
+        }
+
+        if (!foundNext) {
+            // If no unvisited neighbor can be reached, backtrack.
+            path.pop();
+            if (path.length === 0) {
+                // No path found.
+                return [];
+            }
+            current = path[path.length - 1];
+        }
+    }
+    return path;
 }
+
